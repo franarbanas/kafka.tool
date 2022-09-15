30af3f57678fa30c9ca45d0f887137e636a4bdef
franarbanas <fran.arbanas@axilis.com>
2022-09-15 17:30:23 +0200

    added forms instead of custom label+field associations

--- main.go
+++ main.go
@@ -172,70 +172,83 @@ func createChooseProtoDescriptor() *fyne.Container {
 }
 
 func createProduce() *fyne.Container {
-	topicName := widget.NewLabel("Topic name:")
 	topicField := widget.NewEntry()
+
 	marshalerType := widget.NewSelect(
 		[]string{"text", "json"},
 		func(option string) {
 			state.marshalerType = option
 		},
 	)
-
 	marshalerType.SetSelected("json")
-	marshalerTypeName := widget.NewLabel("Marshaler type name:")
 
 	messageValueField := widget.NewMultiLineEntry()
-	messageFieldName := widget.NewLabel("Message:")
-	submitButton := widget.NewButton("Submit", func() {
-		val := []byte(messageValueField.Text)
 
-		if state.chosenProtoFullName != "" {
-			switch state.marshalerType {
-			case "text":
-				err := prototext.Unmarshal(val, &state.chosenProtoMessage)
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{
+				Text:   "Topic name:",
+				Widget: topicField,
+			},
+			{
+				Text:   "Marshaler type:",
+				Widget: marshalerType,
+			},
+			{
+				Text:   "Message:",
+				Widget: messageValueField,
+			},
+		},
+		OnSubmit: func() {
+			val := []byte(messageValueField.Text)
+
+			if state.chosenProtoFullName != "" {
+				switch state.marshalerType {
+				case "text":
+					err := prototext.Unmarshal(val, &state.chosenProtoMessage)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+
+				case "json":
+					err := protojson.Unmarshal(val, &state.chosenProtoMessage)
+					if err != nil {
+						fmt.Println(err)
+						return
+					}
+
+				default:
+					fmt.Println("unknown producer type, not going to use protobuf")
+				}
+
+				var err error
+				val, err = proto.Marshal(&state.chosenProtoMessage)
 				if err != nil {
 					fmt.Println(err)
 					return
 				}
-
-			case "json":
-				err := protojson.Unmarshal(val, &state.chosenProtoMessage)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-			default:
-				fmt.Println("unknown producer type, not going to use protobuf")
 			}
 
-			var err error
-			val, err = proto.Marshal(&state.chosenProtoMessage)
+			_, err := producer.ProduceSync(kafkaclients.Message{
+				Metadata: kafkaclients.Metadata{Topic: topicField.Text},
+				Value:    val,
+			})
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
-		}
 
-		_, err := producer.ProduceSync(kafkaclients.Message{
-			Metadata: kafkaclients.Metadata{Topic: topicField.Text},
-			Value:    val,
-		})
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+			fmt.Println("submitted")
+		},
+	}
 
-		fmt.Println("submitted")
-	})
-
-	topicBorder := container.NewBorder(nil, nil, topicName, nil, topicField)
-	marshalerTypeBorder := container.NewBorder(nil, nil, marshalerTypeName, nil, marshalerType)
-	messageBorder := container.NewBorder(nil, nil, messageFieldName, nil, messageValueField)
-	return container.NewVBox(topicBorder, marshalerTypeBorder, messageBorder, submitButton)
+	return container.NewMax(form)
 }
 
 func createConsume() *fyne.Container {
+	topicField := widget.NewEntry()
+
 	newMessageScreen := widget.NewListWithData(
 		stringList,
 		func() fyne.CanvasObject {
@@ -245,39 +258,46 @@ func createConsume() *fyne.Container {
 			o.(*widget.Label).Bind(i.(binding.String))
 		},
 	)
-	topicName := widget.NewLabel("Topic name:")
-	topicField := widget.NewEntry()
-	consumeButton := widget.NewButton("Consume", func() {
-		go func() {
-			consumer = newKafkaConsumer(state.brokers, topicField.Text, 0, 1, "")
-			for {
-				kafkaMessages, _, err := consumer.Consume(context.TODO(), 0, 0, make(map[string]interface{}))
-				if err != nil {
-					fmt.Println(err)
-				}
 
-				for message := range kafkaMessages {
-					val := string(message.Value)
-					if state.chosenProtoFullName != "" {
-						byteVal, err := r.Unmarshal(message.Value, state.chosenProtoMessage)
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{
+				Text:   "Topic name:",
+				Widget: topicField,
+			},
+		},
+		OnSubmit: func() {
+			go func() {
+				consumer = newKafkaConsumer(state.brokers, topicField.Text, 0, 1, "")
+				for {
+					kafkaMessages, _, err := consumer.Consume(context.TODO(), 0, 0, make(map[string]interface{}))
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					for message := range kafkaMessages {
+						val := string(message.Value)
+						if state.chosenProtoFullName != "" {
+							byteVal, err := r.Unmarshal(message.Value, state.chosenProtoMessage)
+							if err != nil {
+								fmt.Println(err)
+								return
+							}
+
+							val = string(byteVal)
+						}
+
+						err := stringList.Append(val)
 						if err != nil {
 							fmt.Println(err)
 							return
 						}
-
-						val = string(byteVal)
-					}
-
-					err := stringList.Append(val)
-					if err != nil {
-						fmt.Println(err)
-						return
 					}
 				}
-			}
-		}()
-	})
+			}()
+		},
+	}
 
-	topicBorder := container.NewBorder(nil, nil, topicName, nil, topicField)
-	return container.NewBorder(container.NewVBox(topicBorder, consumeButton), nil, nil, nil, newMessageScreen)
+	messagesLabels := widget.NewLabel("Messages:")
+	return container.NewBorder(form, nil, nil, nil, container.NewBorder(nil, nil, messagesLabels, nil, newMessageScreen))
 }
